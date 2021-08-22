c610cc03019ae090bf34aefcb522e89ca179f5ff
OneAutumnMango <84255659+OneAutumnMango@users.noreply.github.com>
2021-08-22 12:33:00 +0100

    Fixed Brackets

--- BuildOptimiserSimplfied.py
+++ BuildOptimiserSimplfied.py
@@ -18,20 +18,29 @@ artifactSubStatRolls = read_yaml(configFileName)["artifactSubStatRolls"]
 def baseStatCombine(key):
     return character[key]+weapon[key]
 
-baseATK = baseStatCombine("ATK")
-baseHP = baseStatCombine("HP")
-baseDEF = baseStatCombine("DEF")
+baseATK = baseStatCombine["ATK"]
+baseHP = baseStatCombine["HP"]
+baseDEF = baseStatCombine["DEF"]
 
-baseATKPercent = baseStatCombine("ATKPercent")
-baseHPPercent = baseStatCombine("HPPercent")
-baseDEFPercent = baseStatCombine("DEFPercent")
+baseATKPercent = baseStatCombine["ATKPercent"]
+baseHPPercent = baseStatCombine["HPPercent"]
+baseDEFPercent = baseStatCombine["DEFPercent"]
 
-baseCritRate = baseStatCombine("CritRate")
-baseCritDMG = baseStatCombine("CritDMG")
-baseEM = baseStatCombine("EM")
-baseEnergyRecharge = baseStatCombine("EnergyRecharge")
+baseCritRate = baseStatCombine["CritRate"]
+baseCritDMG = baseStatCombine["CritDMG"]
+baseEnergyRecharge = baseStatCombine["EnergyRecharge"]
+baseEM = baseStatCombine["EM"]
+
 
 for i in list(artifactMainStatChoice):
     print(artifactMainStatChoice[i]) #prints the artifact main stat choices
     print(artifactMainStats[artifactMainStatChoice[i]]) #prints the value of the choices
           
+print(artifactSubStatRolls)
+maxATKRoll = artifactSubStatRolls["ATKPercent"]
+maxCRRoll = artifactSubStatRolls["CritRate"]
+maxCDRoll = artifactSubStatRolls["CritDMG"]
+maxERRoll = artifactSubStatRolls["EnergyRecharge"]
+maxEMRoll - artifactSubStatRolls["EM"]
+
+rolls = config["rolls"]
